fd4c37be41bf23aa24acad60e06bb616e220edb3
Thiago Ximenes <thiagoximeneslima@gmail.com>
2022-10-09 17:59:37 -0300

    feat: cria config para redirecionar o usuário pra rota de login caso o mesmo receba status 401

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import Router from 'next/router';
+import { parseCookies } from 'nookies';
 
 function getBaseURL() {
   if (process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'local') return 'http://localhost:3333';
@@ -7,15 +9,20 @@ function getBaseURL() {
   return 'http://localhost:3333';
 }
 
-function getToken() {
-  return null;
-}
-
 export const api = axios.create({
   baseURL: getBaseURL(),
 });
 
-api.interceptors.request.use((config) => {
-  config.headers = { Authorization: getToken() as unknown as string | number | boolean };
-  return config;
-});
+api.interceptors.request.use(
+  (config) => {
+    const { 'nextauth.token': token } = parseCookies();
+
+    config.headers = { Authorization: `Bearer ${token && token}` };
+
+    return config;
+  },
+  (err) => {
+    if (err.response.status === 401) return Router.push('/');
+    return null;
+  }
+);
