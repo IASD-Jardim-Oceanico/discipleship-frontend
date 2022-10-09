0d6eb4edb21476fff8b030a2fa1cde564f55ef4f
Thiago Ximenes <thiagoximeneslima@gmail.com>
2022-10-09 00:04:34 -0300

    feat: adiciona função de normalização dos dados enviados na requisição para criação de novo usuário

--- services/disciple-maker.ts
+++ services/disciple-maker.ts
@@ -1,13 +1,15 @@
+import { DiscipleMakerDTO } from '../interfaces';
 import { api } from './api';
 
-type DiscipleMakerDTO = {
-  email: string | undefined;
-  password: string | undefined;
-  role: string;
-  full_name: string | undefined;
-  phone?: string | undefined;
-};
+function normalizeCreateDiscipleMaker(payload: DiscipleMakerDTO) {
+  const { fullName, ...rest } = payload;
+
+  return {
+    ...rest,
+    full_name: fullName,
+  };
+}
 
 export function createDiscipleMaker(discipleMaker: DiscipleMakerDTO) {
-  return api.post('/disciple-maker', discipleMaker);
+  return api.post('/disciple-maker', normalizeCreateDiscipleMaker(discipleMaker));
 }
