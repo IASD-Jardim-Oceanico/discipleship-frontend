99884b89cf677389972a95aa850ede84412614a0
Thiago Ximenes <thiagoximeneslima@gmail.com>
2022-10-09 18:00:29 -0300

    feat: ajusta lógica para buscar informações do usuário no caso de haver token

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,17 +1,54 @@
-import { createContext, ReactNode } from 'react';
+import Router from 'next/router';
+import { parseCookies, setCookie } from 'nookies';
+import { createContext, ReactNode, useEffect, useState } from 'react';
+import { UserInfo } from '../interfaces';
+import { LoginDiscipleMaker } from '../schemas/loginDiscipleMaker';
+import { loginRequest, recoverUserInfo } from '../services/auth';
+import { decodedUserId } from '../utils/jwt';
 
 type AuthContextType = {
   isAuthenticated: boolean;
+  user: UserInfo | null;
+  login: (payload: LoginDiscipleMaker) => Promise<void>;
 };
 
 type ChildrenProp = {
   children: ReactNode;
 };
 
-const AuthContext = createContext({} as AuthContextType);
+export const AuthContext = createContext({} as AuthContextType);
 
 export function AuthProvider({ children }: ChildrenProp) {
-  const isAuthenticated = false;
+  const [user, setUser] = useState<UserInfo | null>(null);
 
-  return <AuthContext.Provider value={{ isAuthenticated }}>{children}</AuthContext.Provider>;
+  const isAuthenticated = !!user;
+
+  useEffect(() => {
+    const { 'nextauth.token': token } = parseCookies();
+    if (token) {
+      const userId = decodedUserId(token);
+      recoverUserInfo(userId).then((data) => {
+        console.log(data)
+        setUser(data.userInfo);
+      }).catch((err) => {
+        console.error(err)
+      });
+    }
+  }, []);
+
+  async function login(payload: LoginDiscipleMaker) {
+    const { token, userInfo } = await loginRequest(payload);
+
+    const ONE_DAY = 60 * 60 * 24 * 1;
+
+    setCookie(undefined, 'nextauth.token', token as unknown as string, { maxAge: ONE_DAY });
+
+    setUser(userInfo);
+
+    Router.push('/dashboard');
+  }
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, user, login }}>{children}</AuthContext.Provider>
+  );
 }
